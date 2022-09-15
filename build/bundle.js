4e1e1adf6aa0c0e606bb097378e27c13dbeeba02
raffaez <rafaeleasouzas@gmail.com>
2022-09-15 15:35:56 -0300

    first commit

---
+++ build/bundle.js
@@ -0,0 +1,5 @@
+function soma(num1, num2) {
+    var soma = num1 + num2;
+    return soma;
+}
+document.write("5 + 7 = ".concat(soma(5, 7)));
---
+++ src/script.ts
@@ -0,0 +1,6 @@
+function soma(num1: number, num2: number): number{
+    const soma: number = num1 + num2;
+    return soma;
+}
+
+document.write(`5 + 7 = ${soma(5,7)}`);
